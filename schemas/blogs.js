ebbddc645bb1ffa0e0369b05ef04eda6681ce3e2
Divyanshu Prasad <divyanshu9871@gmail.com>
2023-11-08 00:46:51 +0530

    added summary field to blogs schema

--- schemas/blogs.js
+++ schemas/blogs.js
@@ -1,23 +1,27 @@
 export default {
-    name: 'blogs',
-    type: 'document',
-    title: 'Blogs',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title',
-      },
-      {
-        name: 'content',
-        type: 'text',
-        title: 'Content',
-      },
-      {
-        name: 'cover_url',
-        type: 'url',
-        title: 'Cover URL',
-      },
-    ],
-  };
-  
+  name: 'blogs',
+  type: 'document',
+  title: 'Blogs',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'content',
+      type: 'text',
+      title: 'Content',
+    },
+    {
+      name: 'summary',
+      type: 'text',
+      title: 'Summary',
+    },
+    {
+      name: 'cover_url',
+      type: 'url',
+      title: 'Cover URL',
+    },
+  ],
+}
