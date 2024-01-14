fe86ab18cfc934441441077d06e8ea8c72a9872c
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-01-14 23:29:40 +0530

    imports added for new schemas

--- schemas/index.js
+++ schemas/index.js
@@ -4,5 +4,6 @@ import case_studies from './case_studies'
 import medias from './medias'
 import news from './news'
 import articles from './articles'
+import phase from './phase'
 
-export const schemaTypes = [testimonials, blogs, case_studies, medias, news, articles]
+export const schemaTypes = [testimonials, blogs, case_studies, medias, news, articles, phase]
