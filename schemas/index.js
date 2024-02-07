4c5392c00af2ca6e541dd79af3ad809ae60e4727
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-02-07 12:39:27 +0530

    removed unused and unwanted shemas

--- schemas/index.js
+++ schemas/index.js
@@ -2,8 +2,6 @@ import testimonials from './testimonials'
 import blogs from './blogs'
 import case_studies from './case_studies'
 import medias from './medias'
-import news from './news'
-import articles from './articles'
 import phase from './phase'
 
-export const schemaTypes = [testimonials, blogs, case_studies, medias, news, articles, phase]
+export const schemaTypes = [testimonials, blogs, case_studies, medias, phase]
